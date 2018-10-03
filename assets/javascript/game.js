7497eca7a15520cca1a099f2c055eb5ba25aa3dd
KI <iqbal.mohammadk@gmail.com>
2018-10-03 15:38:41 -0400

    moar js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,11 @@ var wordBank = ["fudge", "tacos", "jumpman"];
 var counter = 60;
 var livesLeft = 5;
 var computerChoice;
-var userGuess;
 var randomWord = "";
 var underScores = [];
+var guessedLetters = [];
+var letterChoices = document.getElementsByClassName(letterChoices);
+
 
 
 function startGame() {
@@ -20,7 +22,9 @@ function startGame() {
   };
   //printing the underscores to the screen for the words
 
-  $("underScores").textContent = underScores.join(" ");
+  document.getElementById("underScores").textContent = underScores.join(" ");
+
+  console.log(randomWord);
 }
 
 function countdown() {
@@ -43,29 +47,54 @@ $("#startIt").on("click", function (event) {
 
   var timer = setInterval(countdown, 1000);
   startGame();
-
-});
-//variable for the array. for now im going to add a functionality that will add words to an array for me. this will be commented out after but ill remain for future use.
-
-$(".addIt").on("click", function (event) {
-
-    event.preventDefault();
-    var added = $("#addToBank").val();
-    wordBank.push(added);
-    console.log(wordBank);
-
-    //store on sessionstorage
-    sessionStorage.setItem("wordBank", JSON.stringify(wordBank))
   
-});
 
+  document.onkeyup = function (event1) { 
+    var keyPressed = event1.key;
+    console.log(keyPressed);
+  };
+  //if one of the letters the user selects is in the current word, replace the dash with the letter
+  function letterGuess(letter) {
+    console.log(letter);
+    // check to see if letter has been guessed prior, if no run with game logic
+    if (guessedLetters.includes(letter) === false) {
+      // run game logic
+      // add letter to letterBank
+      guessedLetters.push(letter);
+  
+      // loop over pickedWord and see if any character matches letter I guessed
+      for (var i = 0; i < randomWord.length; i++) {
+        // check if pickedWord[i] === letter
+        if (randomWord[i].toLowerCase() === letter.toLowerCase()) {
+          // swap placeholderArray[i] with pickedWord[i]
+          underScores[i] = randomWord[i];
+        }
+      }
+      checkIncorrect(letter);
+  
+    } else {
+      alert("You've already pressed this letter!");
+    }
+  }
 
-//target the array, randomly select one of the words, and print out a _ for each letter (use a for loop here) and print it to the page.
-
-//if one of the letters the user selects is in the current word, replace the dash with the letter
-
+  
 //else remove a point from the total number of guesses left
 
 //if the user guesses all the letters, on the last letter trigger an ajax request to giphy to pull up a 'you win!' gif
 
 //else trigger an ajax request to play a 'you lose!' gif
+
+});
+
+//THIS IS TO ADD WORDS TO THE ARRAY. PLEASE NOTE THEY WILL ONLY BE SAVED FOR THE SESSION
+// $(".addIt").on("click", function (event) {
+
+//     // event.preventDefault();
+//     var added = $("#addToBank").val();
+//     wordBank.push(added);
+//     console.log(wordBank);
+
+//     //store on sessionstorage
+//   sessionStorage.setItem("wordBank", JSON.stringify(wordBank))
+// });
+  //THIS IS TO ADD WORDS TO THE ARRAY. PLEASE NOTE THEY WILL ONLY BE SAVED FOR THE SESSION
